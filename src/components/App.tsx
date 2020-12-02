c0b37d1cc176d3264119d39dd463790a237679fc
komura <affirman03139@outlook.jp>
2020-12-02 23:33:34 +0900

    feat(redux): dispatchテスト

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { increment, decrement } from '../actions'
 
-const App = () => {
-  const hello = <p>Hello React!</p>
-  const input = (
-    <input
-      type="button"
-      value="click me"
-      onClick={() => {
-        console.log('I am clicked')
-      }}
-    />
-  )
-  const profiles = [{ name: 'Taro', age: 3 }, { name: 'Sake', age: 20 }, {}]
-  return (
-    <React.Fragment>
-      {profiles.map((profile) => {
-        return <User name={profile.name} age={profile.age} />
-      })}
-      {hello}
-      {input}
-    </React.Fragment>
-  )
+interface Props {
+  value: number
+  increment: typeof increment
+  decrement: typeof decrement
 }
 
 const User = (props: { name?: string; age?: number }) => {
@@ -36,4 +21,40 @@ User.defaultProps = {
   age: '0',
 }
 
-export default App
+class App extends Component<Props> {
+  hello = (<p>Hello React!</p>)
+  input = (
+    <input
+      type="button"
+      value="click me"
+      onClick={() => {
+        console.log('I am clicked')
+      }}
+    />
+  )
+  profiles = [{ name: 'Taro', age: 3 }, { name: 'Sake', age: 20 }, {}]
+  render() {
+    return (
+      <React.Fragment>
+        {this.hello}
+        {this.input}
+        {this.profiles.map((profile) => {
+          return <User name={profile.name} age={profile.age} />
+        })}
+        <div>value: {this.props.value}</div>
+        <button onClick={this.props.increment}>増やす</button>
+        <button onClick={this.props.decrement}>減らす</button>
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = (state: { count: { value: any } }) => ({
+  value: state.count.value,
+})
+const mapDispatchToProps = {
+  increment,
+  decrement,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
