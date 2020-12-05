e9415219dc630a79b8e04bced9f4b0fd48b6138d
komura <affirman03139@outlook.jp>
2020-12-05 20:37:37 +0900

    fix(url): apiのエンドポイントを変更

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -4,7 +4,7 @@ export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENT = 'CREATE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
-const QUERYSTRING = '?token=token123'
+const QUERYSTRING = '?token=token5555'
 
 export const readEvents = () => async (
   dispatch: (arg0: { type: string; response: Response }) => void,
